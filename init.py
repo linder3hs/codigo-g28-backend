188bbdef78e2614559df1f2bd16cbc42077d926b
linder3hs <linderhassinger00@gmail.com>
2026-01-08 19:52:48 -0500

    if

--- init.py
+++ init.py
@@ -1,5 +1,5 @@
 nombre = "Linder"
-edad = input("Ingrese su edad: ") # todo es un string
+edad = int(input("Ingrese su edad: ")) # todo es un string (str)
 # cuando una variable es conformada por mas de una palabra usamo "_"
 es_mayor_edad = True | False
 # esMayorEdad = True
@@ -9,8 +9,16 @@ saldo = 10.57
 pueden colocar un comentario super largo.
 para ejecutar el archivo usamos el comando python init.py
 """
+
+mensaje = ""
+
+if edad >= 18:
+  mensaje = "Es mayor de edad"
+else:
+  mensaje = "Es menor de edad"
+
 print("---- Mensaje ----")
 print(nombre)
-print(edad)
+print(edad, mensaje)
 print(es_mayor_edad)
 print(saldo)
