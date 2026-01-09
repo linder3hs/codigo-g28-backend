d296572e4d30944f8dcee94f9c0d8977f6faa87e
linder3hs <linderhassinger00@gmail.com>
2026-01-08 23:07:30 -0500

    problemas

--- problema10.py
+++ problema10.py
@@ -2,10 +2,24 @@
 Enunciado: Crea una función es_primo(numero) que determine si un número es primo.
 """
 
+# 4
+# 1 por defecto
+
+# range(2,4) 2,3
+# numero (4) % i (2) == 0
+
+# 5
+# 1 por defecto
+# range(2,5) 2,3,4
+# numero (5) % i (2) == 0 (1)
+# numero (5) % i (3) == 0 (1)
+# numero (5) % i (4) == 0 (1)
+
+
 def es_primo(numero):
     if numero < 2:
         return f"El {numero}, no es primo"
-    
+     
     for i in range(2, numero):
         if numero % i == 0:
             return f"El {numero}, no es primo"
