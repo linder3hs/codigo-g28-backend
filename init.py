93589cce5d15a16b7693b0e98733aae98f9b025e
linder3hs <linderhassinger00@gmail.com>
2026-01-08 19:47:42 -0500

    initial commit :fire:

---
+++ init.py
@@ -0,0 +1,16 @@
+nombre = "Linder"
+edad = input("Ingrese su edad: ") # todo es un string
+# cuando una variable es conformada por mas de una palabra usamo "_"
+es_mayor_edad = True | False
+# esMayorEdad = True
+saldo = 10.57
+
+"""
+pueden colocar un comentario super largo.
+para ejecutar el archivo usamos el comando python init.py
+"""
+print("---- Mensaje ----")
+print(nombre)
+print(edad)
+print(es_mayor_edad)
+print(saldo)
