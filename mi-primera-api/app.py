af0a98d09fad579d18b567967e6190f9e0652de8
linder3hs <linderhassinger00@gmail.com>
2026-01-23 20:47:52 -0500

    flask

--- mi-primera-api/app.py
+++ mi-primera-api/app.py
@@ -1,30 +1,26 @@
 # importar Flask
-from flask import Flask, jsonify, request, session
-
-# import la db y la tabla tareas
-from models import db, Tarea
-# importar flask-migrate
-from flask_migrate import Migrate
-
-# instanciar Flask
-# __name__ == __main__
-app = Flask(__name__)
-
-db.init_app(app)
-
-# instaciar Migrate
-migrate = Migrate(app, db)
+from flask import Flask
+from config import config
+from extensions import db, migrate
+from routes import auth_bp, tareas_bp
 
 
+def create_app(config_name='default'):
+    # instanciar Flask
+    # __name__ == __main__
+    app = Flask(__name__)
+    # estamos cargando la configuracion
+    app.config.from_object(config[config_name])
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(tareas_bp)
+    return app
 
 # iniciar un servidor donde se ejecute
 # debug=True Modo desarrollo, por ende el servidor se reinicia solo
 # se requiere hacer una configuración extra para que nuestras tablas se creen de forma automatica
 if __name__ == "__main__":
-    # crear las tablas
-    # with app.app_context():
-    #     db.create_all()
-    #     print("Base de datos conectada!")
-    #     print("Tablas creadas!")
-    # Hemos comentado esta seccion porque ahora Migrate se encarga de la DB
+    app = create_app('development')
     app.run(debug=True)
