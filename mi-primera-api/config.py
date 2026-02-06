ecf1bd128b622857c9cccef32c046b96eabe3a0c
linder3hs <linderhassinger00@gmail.com>
2026-02-05 20:05:15 -0500

    flask

--- mi-primera-api/config.py
+++ mi-primera-api/config.py
@@ -24,6 +24,10 @@ class Config:
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = 3600 # 1 HORA en segundos
 
+    # Resend
+    RESEND_API_KEY = os.getenv('RESEND_API_KEY')
+    EMAIL_FROM = os.getenv('EMAIL_FROM')
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
