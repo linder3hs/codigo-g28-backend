27d1822c5e2eb90a6cac4568a5ff7707082124df
linder3hs <linderhassinger00@gmail.com>
2026-02-13 21:32:27 -0500

    django

---
+++ django-todo-api/todos/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Todo
+from django.contrib.auth.models import User
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    """
+    Clase que simular ser el to_dict() de Flask
+    """
+    user = serializers.StringRelatedField(read_only=True)
+    class Meta:
+        model = Todo
+        fields = [
+            'id',
+            'title',
+            'description',
+            'category',
+            'completed',
+            'createad_at',
+            'user'
+          ]
+        read_only_fields = ['id', 'created_at', 'user']
