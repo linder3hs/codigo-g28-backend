dc002ec185d4e37de2b4ce1cb864dc5c673dd1e4
linder3hs <linderhassinger00@gmail.com>
2026-02-13 22:57:36 -0500

    django

--- django-todo-api/config/settings.py
+++ django-todo-api/config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', # DJangoRestFramework
-    'todos' # Nuestro todos
+    'todos', # Nuestro todos
+    'users'
 ]
 
 MIDDLEWARE = [
---
+++ django-todo-api/users/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'first_name',
+            'email',
+            'date_joined'
+        ]
+        read_only_fields = ['id', 'date_joined']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+        style={'input_type': 'password'}
+    )
+    password_confirmation = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'first_name',
+            'email',
+            'password_confirmation',
+            'date_joined'
+        ]
+        extra_kwargs = {
+            'fist_name': {'required': True},
+            'email': {'required': True},
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirmation']:
+            raise serializers.ValidationError({
+                "password": "El password no coincide"
+            })
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirmation')
+        user = User.objects.create(**validated_data)
+        return user
