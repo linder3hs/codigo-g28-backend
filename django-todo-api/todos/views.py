58d4eeffab475086b4c6d3aad3b19884068be5b4
linder3hs <linderhassinger00@gmail.com>
2026-02-13 21:46:22 -0500

    django

--- django-todo-api/todos/views.py
+++ django-todo-api/todos/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    Va a generar de forma automatica GET, POST, PUT/PATCH, DELETE
+    """
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    # Filtrar las tareas por usuario
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
+
+    # Cuando se cree un tarea, le asignamos el usuario authenticated
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
