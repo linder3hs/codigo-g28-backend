f0be35f170ca7915e8a69cdfddf0aa2086d81a17
linder3hs <linderhassinger00@gmail.com>
2026-02-19 19:30:37 -0500

    django auth

--- django-todo-api/users/views.py
+++ django-todo-api/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer, UserSerializer
 
-# Create your views here.
+# Registrar usuarios (POST)
+class RegisterView(generics.CreateAPIView):
+    # Solo tiene el endpoint create(POST)
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+
+# Perfiles de usuarios (GET, PUT)
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
