13e4498d06294332bd12e28bf9506722d024c8c2
linder3hs <linderhassinger00@gmail.com>
2026-02-19 22:59:24 -0500

    django

--- django-todo-api/todos/urls.py
+++ django-todo-api/todos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TodoViewSet
+from .views import TodoViewSet, CategoryViewSet
 
 router = DefaultRouter()
 router.register(r'', TodoViewSet, basename='todo')
+router.register(r'categories', CategoryViewSet, basename='category')
 
 # Django tiene la regla que todas las URLs deben crearse en la
 # variable urlpatterns
