83224a8225d9dfb6721d360bbf5408d41fc6ac01
linder3hs <linderhassinger00@gmail.com>
2026-02-19 22:22:07 -0500

    django

--- django-todo-api/todos/serializers.py
+++ django-todo-api/todos/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
-from .models import Todo
-from django.contrib.auth.models import User
+from .models import Todo, Category
 
+
+class CategorySerializer(serializers.ModelSerializer):
+    created_by = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Category
+        fields = [
+            'id',
+            'name',
+            'description',
+            'created_by',
+            'created_at'
+        ]
 
 
 class TodoSerializer(serializers.ModelSerializer):
@@ -9,6 +21,14 @@ class TodoSerializer(serializers.ModelSerializer):
     Clase que simular ser el to_dict() de Flask
     """
     user = serializers.StringRelatedField(read_only=True)
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        source='category',
+        queryset=Category.objects.all(),
+        write_only=True,
+        required=False,
+        allow_null=True
+    )
     class Meta:
         model = Todo
         fields = [
@@ -16,6 +36,7 @@ class TodoSerializer(serializers.ModelSerializer):
             'title',
             'description',
             'category',
+            'category_id',
             'completed',
             'created_at',
             'user'
