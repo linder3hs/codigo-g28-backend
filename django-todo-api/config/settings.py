73af19645c94ba99cb76587e984e0a4a4c0f407f
linder3hs <linderhassinger00@gmail.com>
2026-02-20 20:28:13 -0500

    django

--- django-todo-api/config/settings.py
+++ django-todo-api/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework', # DJangoRestFramework
     'drf_spectacular',
+    'django_filters',
     'todos', # Nuestro todos
     'users'
 ]
@@ -128,7 +129,15 @@ REST_FRAMEWORK = {
    'DEFAULT_PERMISSION_CLASSES': [
       'rest_framework.permissions.IsAuthenticated'
    ],
-   'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+   'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+   # Filtros y paginacion globales
+   'DEFAULT_FILTER_BACKENDS': [
+      'django_filters.rest_framework.DjangoFilterBackend',
+      'rest_framework.filters.SearchFilter',
+      'rest_framework.filters.OrderingFilter',
+   ],
+   'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+   'PAGE_SIZE': 10
 }
 
 SPECTACULAR_SETTINGS = {
