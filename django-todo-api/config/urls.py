da5f6a1c354a3fe0c4f687c8c32d2bef29ab4932
linder3hs <linderhassinger00@gmail.com>
2026-02-20 19:48:00 -0500

    django

--- django-todo-api/config/urls.py
+++ django-todo-api/config/urls.py
@@ -16,11 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # TODO CRUD
     path('api/todos/', include('todos.urls')),
     # REGISTER AND PROFILE
-    path('api/users/', include('users.urls'))
+    path('api/users/', include('users.urls')),
+    # DOCUMENTATION
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger')
 ]
