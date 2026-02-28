2fd59d36c4728b781c8f80345a756fc659087790
linder3hs <linderhassinger00@gmail.com>
2026-02-27 22:13:23 -0500

    django

--- django-todo-api/users/tests/test_serializers.py
+++ django-todo-api/users/tests/test_serializers.py
@@ -19,3 +19,27 @@ class TestRegisterSerializer:
         }
         serializer = RegisterSerializer(data=data)
         assert serializer.is_valid(), serializer.errors
+
+    def test_password_dont_match(self):
+        data = {
+            "username": "nuevo_user",
+            "email": "nuevo_user@test.com",
+            "first_name": "Nuevo",
+            "password": "SecurePwd123!",
+            "password_confirmation": "OtherPwd123!"
+        }
+        serializer = RegisterSerializer(data=data)
+        assert not serializer.is_valid()
+        assert "password" in serializer.errors
+    
+    def test_password_rejected(self):
+        data = {
+            "username": "nuevo_user",
+            "email": "nuevo_user@test.com",
+            "first_name": "Nuevo",
+            "password": "123",
+            "password_confirmation": "123"
+        }
+        serializer = RegisterSerializer(data=data)
+        assert not serializer.is_valid()
+        assert "password" in serializer.errors
