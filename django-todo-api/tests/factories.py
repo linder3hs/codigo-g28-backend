1da1bf35cea55e66434b7d403d4f22e40a15e12e
linder3hs <linderhassinger00@gmail.com>
2026-02-27 19:37:12 -0500

    django

--- django-todo-api/tests/factories.py
+++ django-todo-api/tests/factories.py
@@ -12,8 +12,28 @@ class UserFactory(factory.django.DjangoModelFactory):
     """
     class Meta:
         model = User
-    
+
     username = factory.Sequence(lambda n: f"user_{n}")
     email = factory.LazyAttribute(lambda obj: f"{obj.username}@test.com")
     first_name = factory.Faker('first_name')
     password = factory.PosstGenerationMethodCall('set_password', 'Test1234!')
+
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Category
+
+    name = factory.Sequence(lambda n: f"Category {n}")
+    description = factory.Faker('sentence')
+    created_by = factory.SubFactory(UserFactory)
+
+
+class TodoFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Todo
+
+    title = factory.faker('sentence', nb_words=4)
+    description = factory.Faker('paragraph')
+    completed = False
+    user = factory.SubFactory(UserFactory)
+    category = factory.SubFactory(CategoryFactory)
